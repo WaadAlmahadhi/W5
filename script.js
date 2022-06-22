1928643f09cce2f474cc041fb8ae9d683957c038
WaadAlmahadhi <wa3dalwad3y@gmail.com>
2022-06-22 22:55:31 +0300

    addall

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+function hilighte1()
+{   var element=document.getElementById("one");
+    element.style.backgroundColor = "yellow";
+    element.style.color = "black";
+    
+}
+
+function hilighte2()
+{   
+    var element=document.getElementById("two");
+        element.style.backgroundColor = "yellow";
+        element.style.color = "black";
+}
+function hilighte3()
+{   
+    var element=document.getElementById("three");
+        element.style.backgroundColor = "yellow";
+        element.style.color = "black";
+}
+function hilighte4()
+{   
+    var element=document.getElementById("four");
+        element.style.backgroundColor = "yellow";
+        element.style.color = "black";
+}
+function hilighte5()
+{   
+    var element=document.getElementById("five");
+        element.style.backgroundColor = "yellow";
+        element.style.color = "black";
+}
+function hilighte6()
+{   
+    var element=document.getElementById("six");
+        element.style.backgroundColor = "yellow";
+        element.style.color = "black";
+}
+function hilighte7()
+{   
+    var element=document.getElementById("seven");
+        element.style.backgroundColor = "yellow";
+        element.style.color = "black";
+}
